4bd284b1ba473b528025e1389e57951413574cbb
Siirtajamestari <siirtajamestari@gmail.com>
2023-03-18 21:33:24 +0300

    Version 0.0.4. Added simple GUI.

--- main.py
+++ main.py
@@ -1,82 +1,88 @@
-import re
-
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import keyboard
-import time
+import tkinter as tk
+from tkinter import messagebox
 
-while True:
-    choise = input("Do you want to search for space news or videos? \n"
-                   "News - for news, videos - for videos: ")
-    if choise == "news" or choise == "News":
-        pagecount = int(input("How many pages do you want to parse (max 9)?: "))
-        newsFile = open("news.txt", "w", encoding="utf-8")
-        driver = webdriver.Chrome()
-        dictionary = {
-            'class': ['listingResult small result1', 'listingResult small result2', 'listingResult small result3',
-                      'listingResult small result4', 'listingResult small result5', 'listingResult small result6',
-                      'listingResult small result7', 'listingResult small result8', 'listingResult small result9',
-                      'listingResult small result10', 'listingResult small result11', 'listingResult small result12',
-                      'listingResult small result13', 'listingResult small result14', 'listingResult small result15',
-                      'listingResult small result16', 'listingResult small result17', 'listingResult small result18',
-                      'listingResult small result19', 'listingResult small result20', 'listingResult small result21']}
-        for i in range(1, pagecount + 1):
-            url = f"https://www.space.com/news/{i}"
-            driver.get(url)
-            scroll_count = 5
+def update_mode():
+    if mode_var.get():
+        mode_label.config(text="News mode")
+        search_button.pack_forget()
+        pagecount_entry.pack()
+        search_entry.pack_forget()
+        search_button.pack()
+        search_button.config(command=search_news)
+    else:
+        mode_label.config(text="Video mode")
+        search_button.pack_forget()
+        pagecount_entry.pack_forget()
+        search_entry.pack()
+        search_button.pack()
+        search_button.config(command=search_videos)
 
-            soup = BeautifulSoup(driver.page_source, "html.parser")
-            for articleParent in soup.find_all("div", dictionary):
-                articleDeal = articleParent.find(class_='category-link')
-                if articleDeal is not None:
-                    continue
-                articleChild = articleParent.find("a", class_='article-link')
-                if articleChild is not None and 'href' in articleChild.attrs:
-                    articleName = articleChild['aria-label']
-                    articleUrl = articleChild['href']
-                    newsFile.write(f"{articleName} | {articleUrl}\n")
+def search_news():
+    pagecount = int(pagecount_entry.get())
+    if not 1 <= pagecount <= 9:
+        messagebox.showerror("Error", "Please enter a number between 1 and 9.")
+        return
 
-        driver.quit()
-        newsFile.close()
-        break
-    elif choise == "Videos" or choise == "videos":
-        search = input("Enter your search: ")
-        url = f"https://www.youtube.com/results?search_query={search}"
-        # url = "http://hdrezkasrskk.net/"
-        videosFile = open("videos.txt", "w", encoding="utf-8")
-        driver = webdriver.Chrome()
+    newsFile = open("news.txt", "w", encoding="utf-8")
+    driver = webdriver.Chrome()
+
+    for i in range(1, pagecount + 1):
+        url = f"https://www.space.com/news/{i}"
         driver.get(url)
-
-        SCROLL_PAUSE_TIME = 2
-
-        # Get scroll height
-        last_height = driver.execute_script("return document.body.scrollHeight")
-
-        # while True:
-        # Scroll down to bottom
-        # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-        # Wait to load page
-        # time.sleep(SCROLL_PAUSE_TIME)
-
-        # Calculate new scroll height and compare with last scroll height
-        # new_height = driver.execute_script("return document.body.scrollHeight")
-        # if new_height == last_height:
-        # break
-        # last_height = new_height
-        # driver.execute_script("window.scrollTo(0, 1080)")
+        scroll_count = 5
 
         soup = BeautifulSoup(driver.page_source, "html.parser")
+        for articleParent in soup.find_all("div", {"class": "listingResult small"}):
+            articleDeal = articleParent.find(class_='category-link')
+            if articleDeal is not None:
+                continue
+            articleChild = articleParent.find("a", class_='article-link')
+            if articleChild is not None and 'href' in articleChild.attrs:
+                articleName = articleChild['aria-label']
+                articleUrl = articleChild['href']
+                newsFile.write(f"{articleName} | {articleUrl}\n")
 
-        for video in soup.select('a#video-title'):
-            if 'href' in video.attrs:
-                videoName = video['title']
-                videoUrl = f"https://www.youtube.com{video['href']}"
-                videosFile.write(f"{videoName} | {videoUrl}\n")
+    driver.quit()
+    newsFile.close()
 
-        driver.quit()
-        videosFile.close()
-        break
-    else:
-        print("Incorrect user input, looping back.")
-        continue
+def search_videos():
+    url = f"https://www.youtube.com/results?search_query={search_entry.get()}"
+    videosFile = open("videos.txt", "w", encoding="utf-8")
+    driver = webdriver.Chrome()
+    driver.get(url)
+
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+
+    for video in soup.select('a#video-title'):
+        if 'href' in video.attrs:
+            videoName = video['title']
+            videoUrl = f"https://www.youtube.com{video['href']}"
+            videosFile.write(f"{videoName} | {videoUrl}\n")
+
+    driver.quit()
+    videosFile.close()
+
+window = tk.Tk()
+window.title("DaddyScrapeLegs")
+window.geometry("250x150")
+
+mode_label = tk.Label(window, text="News mode", font=("Aerial", 16))
+mode_label.pack()
+
+mode_var = tk.BooleanVar()
+mode_var.set(True)
+mode_switch = tk.Checkbutton(window, text="Switch mode", variable=mode_var, command=update_mode)
+mode_switch.pack()
+
+pagecount_entry = tk.Entry(window, width=30, validate="key")
+pagecount_entry.config(validatecommand=(pagecount_entry.register(lambda s: s.isdigit() and int(s) in range(1, 10)), "%P"))
+pagecount_entry.pack()
+
+search_entry = tk.Entry(window, width=30)
+search_button = tk.Button(window, text="Search", width=10)
+search_button.pack()
+search_button.config(command=search_news)
+
+window.mainloop()
